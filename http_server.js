228cd8ff24a61fc6788a3615348274802730b27b
abelsan <abel@mit.edu>
2020-10-13 10:03:15 -0400

    first commit

---
+++ http_server.js
@@ -0,0 +1,66 @@
+// add http server
+// -----------------------
+// YOUR CODE
+
+var low     = require('lowdb');
+var fs      = require('lowdb/adapters/FileSync');
+var adapter = new fs('db.json');
+var db      = low(adapter);
+
+// configure express to serve static files from public directory
+// ------------------------------------------------------------------
+// YOUR CODE
+
+// init the data store
+db.defaults({ posts: []}).write();
+
+// list posts
+app.get('/data', function(req, res){     
+
+    // YOUR CODE
+
+});
+
+// ----------------------------------------------------
+// add post - test using:
+//      curl http://localhost:3000/posts/ping/1/false
+// ----------------------------------------------------
+app.get('/posts/:title/:id/:published', function(req, res){
+
+    // YOUR CODE
+
+});
+
+// ----------------------------------------------------
+// filter by published state - test using:
+//      curl http://localhost:3000/published/true
+// ----------------------------------------------------
+app.get('/published/:boolean', function(req, res){
+
+    // YOUR CODE
+
+});
+
+// ----------------------------------------------------
+// update published value - test using:
+//      curl http://localhost:3000/published/1/true
+// ----------------------------------------------------
+app.get('/published/:id/:boolean', function(req, res){
+
+    // YOUR CODE
+
+});
+
+// ----------------------------------------------------
+// delete entry by id - test using:
+//      curl http://localhost:3000/delete/5
+// ----------------------------------------------------
+app.get('/delete/:id/', function(req, res){
+
+    // YOUR CODE
+
+});
+
+// start server
+// -----------------------
+// YOUR CODE
---
+++ simple.js
@@ -0,0 +1,28 @@
+var low     = require('lowdb');
+var fs      = require('lowdb/adapters/FileSync');
+var adapter = new fs('db.json');
+var db      = low(adapter);
+
+// init the data store
+// ---------------------------
+// YOUR CODE
+
+// add post
+// ----------------------------
+// YOUR CODE
+
+// count posts
+// ----------------------------
+// YOUR CODE
+
+// find all posts ids
+// ----------------------------
+// YOUR CODE
+
+// all matches of published false
+// ----------------------------
+// YOUR CODE
+
+// find post with published false
+// ----------------------------
+// YOUR CODE
